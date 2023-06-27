08ff045c768ec7803002f9fad7177b05f8c18231
Kim Kyubin <skipperkim307@gmail.com>
2023-06-27 20:52:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string voca;
+    cin >> voca;
+    int idx;
+    cin >> idx;
+
+    cout << voca[idx - 1] << endl;
+
+    return 0;
+}
