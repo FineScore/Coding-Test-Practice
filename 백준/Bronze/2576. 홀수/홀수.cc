0471d6fdbf2d411e809d2e297922d87aa891de7a
Kim Kyubin <skipperkim307@gmail.com>
2023-07-06 18:27:34 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int sum = 0;
+    int min = 100;
+
+    for (size_t i = 0; i < 7; i++)
+    {
+        int num;
+        cin >> num;
+        if (num % 2 != 0)
+        {
+            sum += num;
+            if (min > num)
+            {
+                min = num;
+            }
+        }
+    }
+
+    if (sum == 0)
+    {
+        cout << -1 << "\n";
+    }
+    else
+    {
+        cout << sum << "\n";
+        cout << min << "\n";
+    }
+}
