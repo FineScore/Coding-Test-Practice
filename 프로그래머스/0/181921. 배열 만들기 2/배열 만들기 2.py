fcd6cac4770efb6d8eaf2fb4cb7ada39fce2421d
Kim Kyubin <skipperkim307@gmail.com>
2025-07-06 20:41:16 +0900

    [level 0] Title: 배열 만들기 2, Time: 658.60 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,14 +1,6 @@
 def solution(l, r):
     answer = []
-    f = True
     for i in range(l, r + 1):
-        for s in str(i):
-            if (not s == "0") and (not s == "5"): 
-                f = False
-                break
-        if f == True:
+        if not set(str(i)) - set(['0', '5']):
             answer.append(i)
-        f = True
-    if len(answer) == 0:
-        answer.append(-1)
-    return answer
+    return answer if answer else [-1]
