d2a0ef0d4b8f21b15feeb9339340631b0c3e7b7d
Kim Kyubin <skipperkim307@gmail.com>
2025-07-09 17:21:52 +0900

    [level 1] Title: 소수 찾기, Time: 290.50 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,7 @@
 def solution(n):
-    count = 0
-    for i in range(1, n + 1):
-        if i < 2:
-            continue
-        isSosu = True
-        for j in range(2, int(i ** (1/2) + 1)):
-            if i % j == 0:
-                isSosu = False
-                break
-        if isSosu:
-            count += 1
-    return count
+    t = set(range(2, n + 1))
+    
+    for i in range(2, int(n ** (1/2) + 1)):
+        if i in t:
+            t -= set(range(i + i, n + 1, i))
+    return len(t)
