9c9c980305e17e1538e5256a3dceff2ed48b2869
Kim Kyubin <skipperkim307@gmail.com>
2025-07-09 17:11:30 +0900

    [level 1] Title: 소수 찾기, Time: 2996.09 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/README.md
+++ 프로그래머스/1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
 
 ### 성능 요약
 
-메모리: 9.39 MB, 시간: 4036.62 ms
+메모리: 9.36 MB, 시간: 2996.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 09일 17:05:48
+2025년 07월 09일 17:11:26
 
 ### 문제 설명
 
--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,15 +1,13 @@
-def isSosu(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** (1/2) + 1)):
-        if n % i == 0:
-            return False
-    return True
-
 def solution(n):
     count = 0
     for i in range(1, n + 1):
-        if isSosu(i):
+        if i < 2:
+            continue
+        isSosu = True
+        for j in range(2, int(i ** (1/2) + 1)):
+            if i % j == 0:
+                isSosu = False
+                break
+        if isSosu:
             count += 1
-        
     return count
